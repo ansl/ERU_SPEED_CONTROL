286411f95a8d4830d1317a4b3467cc69569a3c70
ansl <antonio.sanz.luengo@gmail.com>
2021-05-28 13:23:33 +0200

    Update PWM_READ.cpp
    restructure of ISR routines and general class structure

--- lib/PWM_READ/PWM_READ.cpp
+++ lib/PWM_READ/PWM_READ.cpp
@@ -3,56 +3,63 @@
 #include <pins_arduino.h>
 #include <PWM_READ.h>
 
-#define PWM_IN_FREQ 32*65536UL/F_CPU  /1960//1.96 kHz
 //extern volatile unsigned long timer0_overflow_count;
 
 
 
 
-        volatile uint32_t      peak_width         =      0;
-        volatile uint16_t      tmr_overf          =      0;
+        
 
-// ISR(INT1_vect){
-//     PWM_READ::PWM_ISR();
-// }
+ISR(INT1_vect){
+    PWM_READ::PWM_ISR();
+}
 
-// ISR(TIMER1_OVF_vect) {
-//    tmr_overf++;  
-//  }
+ISR(TIMER1_OVF_vect) {
+   tmr_overf++;  
+}
 
 PWM_READ::PWM_READ(){
+        volatile uint16_t      peak_width         =0;
+        volatile uint16_t      valley_width       =0;
+        volatile uint16_t      tmr_overf          =0;
+        volatile float         Freq               =0;
+        volatile float         Duty               =0;
+        volatile uint8_t       Duty_256           =0;
+}
+PWM_READ::init(){
     //using Timer 1 
          //inicializo timer1/normal mode
              TCCR1A=0;					//Inicializo los control registers del timer1 tanto el A como el B
              TCCR1B=0;
              TCCR1C=0;
              TIMSK1=0;
-            // TCCR1A |=(1 << COM1B1);		//defino la salida en el OCR1B en toogle mode #pin10
-            // TCCR1A |=(1 << WGM10);		//WGMxx Lo define como fast pwm con el TOP en OCR1A /la comparaci?n en el OC1B
-            // TCCR1A |=(1 << WGM11);
-             TCCR1B |=(1 << ICNC1);     //input noise canceler-> It causes a delay of 4 clock cycles as it considers a change only if it persists for at least 4 successive system clocks.
-             TIMSK1 |=(1 << ICIE1);     //Input Capture Interrupt Enable
-             TCCR1B |=(1 << ICES1);     //Select edge detection for input capture function.0 = Capture on the falling edge1 = Capture on rising edge
-             TCCR1B |=(1 << CS10);	  	//Seteo el prescaling en no prescaling
-             //TCCR1B |=(1 << CS11);		//Seteo el prescaling en no prescaling
 
-             //TIMSK1 |= (1<<TOIE1); // Overflow Interrupt Enable 
-             TIFR1 |=(1<<ICF1);
+        
+             TCCR1B |=(1 << ICNC1);             //input noise canceler-> It causes a delay of 4 clock cycles as it considers a change only if it persists for at least 4 successive system clocks.
+             TCCR1B |=(1 << ICES1);             //Select edge detection for input capture function.0 = Capture on the falling edge1 = Capture on rising edge
+             TCCR1B |=(1 << CS10);	  	//Seteo el prescaling en no prescaling
+        
+             TIMSK1 |=(1 << ICIE1);             //Input Capture Interrupt Enable
+             TIMSK1 |= (1<<TOIE1);              // Overflow Interrupt Enable 
+             TIFR1 |=(1<<ICF1);                 // clear Flag 
 
 }
-uint32_t PWM_READ::duty(){
+
+float PWM_READ::duty(){
         // return 10*lroundf((_range/10)*abs(CYCLE_END-DUTY_START)/CYCLE_END);
-           return peak_width;
+           return Duty;
+
+}
+uint8_t PWM_READ::duty(){
+        // return 10*lroundf((_range/10)*abs(CYCLE_END-DUTY_START)/CYCLE_END);
+           return Duty_256;
 
 }
 float PWM_READ::freq(){
         // return 10*lroundf((_range/10)*abs(CYCLE_END-DUTY_START)/CYCLE_END);
-        return PWM_IN_FREQ;
+        return Freq;
 }
-// unsigned long PWM_READ::test(){
-//         return CYCLE_END;
 
-// }
 void PWM_READ::PWM_ISR(){
     uint32_t curr_v = ICR1;
     static uint32_t last_v;
@@ -61,8 +68,15 @@ void PWM_READ::PWM_ISR(){
 
     if(((TCCR1B>>ICES1)&1)==0 && curr_v>last_v){
       peak_width=curr_v-last_v;
-      tmr_overf=0;
+
     }
+    if(((TCCR1B>>ICES1)&1)==1 && curr_v>last_v){
+      valley_width=curr_v-last_v;
+      Freq=F_CPU/(peak_width+valley_width);
+      Duty=peak_width/(peak_width+valley_width);
+      Duty_256=round(peak_width/(peak_width+valley_width)*255);
+    }
+        
     TCCR1B ^= (1<<ICES1);  // invert flange detection
 
     last_v=curr_v;
